c470b2a445c71a4c4125072215786adcbbea6cde
AMON164 <amutuhaireamon5@gmail.com>
2024-09-18 17:45:26 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{//int x[5];
+    //intx[]={9,8,7,6,5};
+    //cout<<"The element at index 0 is:"<<(x)<<endl;
+    //cout<<"The element at index 4 is:"<<(x)<<endl;
+    //int x [5];
+    //x[0]=9;
+    //x[1]=8;
+    //x[2]=7;
+    //x[3]=6;
+    //x[4]=5; //last Element
+
+    //cout<<"The Element at index 1 is : "<<x[1]<<endl;
+
+    int x[2][3] = {{5,6}, {7,8,9}};
+
+
+
+    cout<<x[1][2];
+
+    return 0;
+}
